4891698be187524f948070d1a765ee5ff57a9f7e
ibtechraj <raj_shk@rediffmail.com>
2019-11-13 17:07:17 +0530

    added 2 funcs for creating book

--- asset/index.js
+++ asset/index.js
@@ -5,7 +5,7 @@ function Book(title, author, numOfPages) {
   this.author = author;
   this.numOfPages = numOfPages;
 
-  this.info = function() {
+  this.info = function () {
     return `${this.title} by ${this.author}, ${this.numOfPages} pages, not read yet`;
   };
 }
@@ -17,3 +17,18 @@ function addBookToLibrary(obj) {
   return myLibrary;
 }
 console.log(addBookToLibrary(newBook));
+
+function templates{
+  myLibrary.map(book =>
+    `<tr>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.numOfPages}</td>
+  </tr>`
+  ).join('');
+}
+
+const render = (temp, node) => {
+  node.innerHTML = temp;
+};
+
